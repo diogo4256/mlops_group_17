ebbc5fad7a6fa9445053c292d5b0c69c89f0c7e8
diogo4256 <diogoadegas@Diogos-MacBook-Pro.local>
2024-01-06 14:37:27 +0100

    Inital kaggle data retrieval

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,3 +1,15 @@
+import zipfile
+from kaggle.api.kaggle_api_extended import KaggleApi
+
 if __name__ == '__main__':
     # Get the data and process it
+    path_extract = "./data/raw"
+    path_zip =f"{path_extract}/zips"
+    api = KaggleApi()
+    api.authenticate()
+    api.dataset_download_files("moltean/fruits" , path_zip)
+    
+    with zipfile.ZipFile(f"{path_zip}/fruits.zip", 'r') as zip_ref:
+    # Extract all contents to the specified directory
+        zip_ref.extractall(path_extract)
     pass
